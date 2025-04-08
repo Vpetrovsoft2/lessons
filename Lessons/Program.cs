932150b7868ce9f66804985480faf5f803e89fdd
Петров Виталий Игоревич <vitalyp@amdocs.ru>
2025-04-08 19:49:57 +0300

    Lesson 5

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -1,18 +1,1 @@
-﻿using Lessons.Lesson4;
-
-Bank bank = new(500, "Виталий", "4124124");
-
-bank.Deposit(50);
-Console.WriteLine(bank.DisplayInfo());
-
-GetWithdraw(600);
-
-GetWithdraw(111);
-
-
-
-void GetWithdraw(decimal amount)
-{
-    bank.Withdraw(amount);
-    Console.WriteLine(bank.DisplayInfo());
-}
+﻿
