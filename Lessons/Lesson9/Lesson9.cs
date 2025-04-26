9601cffd6d77124a09bb54782ffb27457db25a2e
Петров Виталий Игоревич <vitalyp@amdocs.ru>
2025-04-26 14:11:18 +0300

    Lesson 9

---
+++ Lessons/Lesson9/Lesson9.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+namespace Lessons.Lesson9;
+
+public class Lesson9
+{
+    // Многопоточные методы
+    public static void TestThread()
+    {
+        Thread thread = new(DoWork());
+        thread.Start();
+    }
+
+    private Thread DoWork()
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Работа в отдельном потоке завершена.");
+        return new();
+    }
+
+    // Асинхронные методы
+    public static async Task<string> Async()
+    {
+        Console.WriteLine("Загрузка началась....");
+
+        string content = await LoadDataAsync();
+        Thread.Sleep(5000);
+
+        Console.WriteLine("Данные загружены: ");
+        
+        return content;
+    }
+
+    public static async Task<string> LoadDataAsync()
+    {
+        using (var client = new HttpClient())
+        {
+            return await client.GetStringAsync("https://example.com");
+        }
+    }
+}
--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -5,8 +5,29 @@
  Полиморфизм
  Абстракция
  */
-
 using Lessons.Lesson8.Cats;
+using Lessons.Lesson9;
+
+//---------------------------------||-------------------------------------
+// Многопоточный пример
+
+
+//---------------------------------||-------------------------------------
+// Асинхронный пример
+var test1 = Lesson9.Async();
+var test2 = Lesson9.Async();
+var test3 = Lesson9.Async();
+var test4 = Lesson9.Async();
+var test5 = Lesson9.Async();
+
+Task.WaitAll(test1, test2, test3, test4, test5);
+
+string text = test1.Result + test2.Result + test3.Result + test4.Result + test5.Result;
+
+
+
+
+Console.WriteLine();
 
 CatsJournal catsJournal = new();
 
@@ -24,7 +45,6 @@ catCleopatra.GetCoatColor();
 catsJournal.AddCat(catCleopatra);
 
 catsJournal.ShowAllCats();
-
 
 
 
