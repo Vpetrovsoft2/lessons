2cff88ce92825e45f4d847fe0669094602f5f6c6
Петров Виталий Игоревич <vitalyp@amdocs.ru>
2025-04-28 19:52:07 +0300

    First test

---
+++ Autotests/UITests/UnitTest1.cs
@@ -0,0 +1,54 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Autotests.UITests
+{
+    public class Tests : BaseTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Driver.FindElement(By.XPath("//h5[text()='Elements']"));
+
+            var forms = Driver.FindElement(By.XPath("//h5[text()='Forms']"));
+            forms.Click();
+
+            var practiceForm = Driver.FindElement(By.XPath("//span[text()='Practice Form']"));
+            practiceForm.Click();
+
+            var userName = Driver.FindElement(By.CssSelector($"input[id='firstName']"));
+            userName.SendKeys("Vitya");
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Driver.FindElement(By.XPath("//h5[text()='Elements']"));
+
+        }
+
+
+        /* 
+         CSS-селекторы
+         Селектор           Пример
+         tag               input, button, div
+         ID                id="15"  ||   #15
+         class             class="True_class"  || .True_class
+         attribute         placeholder="Current Address"
+         
+         Комбинация        div[id='g']
+         Несколько классов .class1.class2
+         Родитель > потомок  parent > child  ||  div > input
+         Псевдоклассы       div:nth-child(2) || first-child
+
+         
+         XPath - запросы
+
+         /html/body/div[2]/div/div/div[2]/div/div[1]/div/div[3]/h5
+
+         //*[@id="app"]/../div/div[3]/h5
+         */
+
+    }
+}
